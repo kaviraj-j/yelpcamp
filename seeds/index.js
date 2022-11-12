691974b0701f0daa607eecd4879baa06378db855
Kaviraj J <kavirajmailz@gmail.com>
2022-11-12 20:42:09 +0530

    added styles

---
+++ seeds/index.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const randomImage1 = Math.floor(Math.random() * 3);
+        const randomImage2 = Math.floor(Math.random() * 3);
+        const images = [
+            {
+              url: 'https://res.cloudinary.com/dj9gpeweh/image/upload/v1668265201/YelpCamp/m16bqyvanng50bonulxp.jpg',
+              filename: 'YelpCamp/m16bqyvanng50bonulxp'
+            },
+            {
+              url: 'https://res.cloudinary.com/dj9gpeweh/image/upload/v1668265208/YelpCamp/w8eabfajnhbscvrppvym.jpg',
+              filename: 'YelpCamp/w8eabfajnhbscvrppvym'
+            },
+            {
+              url: 'https://res.cloudinary.com/dj9gpeweh/image/upload/v1668265208/YelpCamp/c2txpnb5w6m671v3xce5.jpg',
+              filename: 'YelpCamp/c2txpnb5w6m671v3xce5'
+            }
+          ]
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '63668b7acc2d3e49e4cf71e2',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price,
+            images: [
+                {
+                    url: images[randomImage1].url,
+                    filename: images[randomImage1].filename
+                },
+                {
+                    url: images[randomImage2].url,
+                    filename: images[randomImage2].filename
+                }
+            ]
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
