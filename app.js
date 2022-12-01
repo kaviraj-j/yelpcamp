d55d650fe29cbe6c469077beddee19280905b0f2
Kaviraj J <kavirajmailz@gmail.com>
2022-12-01 20:12:09 +0530

    useless components removed

--- app.js
+++ app.js
@@ -115,6 +115,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Serving')
 })
-
-
-
--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,7 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect('mongodb+srv://kaviraj-j:UdGNHdhvfYUBWx7n@cluster0.isyqon2.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -21,7 +21,8 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    console.log('Deleted');
+    for (let i = 0; i < 15; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const randomImage1 = Math.floor(Math.random() * 3);
         const randomImage2 = Math.floor(Math.random() * 3);
@@ -41,7 +42,7 @@ const seedDB = async () => {
           ]
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '63668b7acc2d3e49e4cf71e2',
+            author: '63735114ed56550016b84c78',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
